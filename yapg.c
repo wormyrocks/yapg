3abf7563353e1d252fe66df04b6e9de47fddf9c0
Evan Kahn <ekahn@hmc.edu>
2017-05-03 17:13:23 -0700

    Added better commenting

--- yapg.c
+++ yapg.c
@@ -10,9 +10,9 @@
 
 #define PI 3.14159265
 
-#define LEDOUT 1
-#define DEBUG 0
-#define RPM 500
+#define LEDOUT 1 // turn on for LED output
+#define DEBUG 0  // turn on for debug prints
+#define RPM 500  // set rotation speed here
 
 unsigned char pixels, revs, total_frames, speed;
 
@@ -28,6 +28,9 @@ struct pixel *getPixel(struct pixel *frames, unsigned i, unsigned j, unsigned k)
 	return frames + addr;
 }
 
+// details on gamma correction here:
+// https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+
 unsigned char gamma_correct(unsigned char color){
 	int corrected = pow(color,3)/(255*255);
 	return (unsigned char) corrected;
@@ -36,12 +39,15 @@ unsigned char gamma_correct(unsigned char color){
 
 void setLED (struct pixel *p){
 	if (LEDOUT){
-		spiSendReceive(0xff);
+		spiSendReceive(0xff); // full brightness; sending partial brightness on APA102 is pointless
 		spiSendReceive(gamma_correct(p->b));
 		spiSendReceive(gamma_correct(p->g));
 		spiSendReceive(gamma_correct(p->r));
 	}
 }
+
+// LED information here:
+// https://cdn-shop.adafruit.com/datasheets/APA102.pdf
 
 void initFrame (){
 	int i;
@@ -49,7 +55,7 @@ void initFrame (){
 }
 
 void endFrame (){
-	if (LEDOUT){
+	if (LEDOUT){ // end SPI frame
 		spiSendReceive(0xff);
 		spiSendReceive(0xff);
 		spiSendReceive(0xff);
@@ -75,7 +81,7 @@ void lightUp(struct pixel *frames, int revnum, int framenum){
 
 }
 
-void sighandler (int sig){
+void sighandler (int sig){ // if user hits ctrl-C during operation, shut down the globe
 		if (LEDOUT){
 			digitalWrite(22, 0);
 			delayMicros(100000);
@@ -86,7 +92,7 @@ void sighandler (int sig){
 			p.b = 0x00;
 			initFrame();
 			for (i = 0; i < pixels; ++i){
-				setLED(&p);
+				setLED(&p); // turn off all LEDs
 			}
 			endFrame();
 		}
@@ -96,12 +102,12 @@ void sighandler (int sig){
 int main() {
 	signal(SIGINT, sighandler);
 	unsigned int time_1;
-	if (LEDOUT){
+	if (LEDOUT){ // if we're writing to LEDs and not debugging
 		pioInit();
-		spiInit(10000000);
-		pinMode(22, OUTPUT);
-		pinMode(27, OUTPUT);
-		digitalWrite(27, 1);
+		spiInit(10000000); // initialize SPI output to LED strip
+		pinMode(22, OUTPUT); // pin 22 tells the FPGA to turn the motor on
+		pinMode(27, OUTPUT); // pin 27 resets the FPGA
+		digitalWrite(27, 1); // reset the FPGA
 		delayMicros(10000);
 		digitalWrite(27, 0);
 	}
@@ -136,7 +142,7 @@ int main() {
 			exit(1);
 		}
 		memset(frames, 0x00, size);
-		fread(frames, sizeof(char), size, fp);
+		fread(frames, sizeof(char), size, fp); // load frame data into memory
 		done = 1;
 
 	}else{
@@ -146,9 +152,9 @@ int main() {
 
 	digitalWrite(22,1);
 
-	int revnum = revs - 1;
-	int delay = 1000000 * 60 / (revs * RPM);
-	int framenum = 0;
+	int revnum = revs - 1; // revs counts the number of vertical lines in one full revolution
+	int delay = 1000000 * 60 / (revs * RPM); // constant delay based on motor speed and revs
+	int framenum = 0; // if we are displaying an animation, keep track of what frame we're on
 	while(1){
 		time_1 = getTime();
 		lightUp(frames,revnum, framenum);
@@ -159,11 +165,11 @@ int main() {
 		if (revnum == 0){
 			++revolutions;
 		}
-		if (revolutions == speed){
+		if (revolutions == speed){ // speed variable controls the number of times one frame of an animation is displayed 
 			revolutions = 0;
-			framenum = ((framenum + 1)%total_frames);
+			framenum = ((framenum + 1)%total_frames); // increment frame
 		}
-		while (getTime() < time_1 + delay){};
+		while (getTime() < time_1 + delay){}; // wait until next line should be displayed
 	}
 
 	printf("\n");
